39a06bc1516c05e36e71b88900458f81529adddc
Łukasz Burski <lukasz.burski@lppsa.com>
2026-04-07 09:50:29 +0200

    bright theme

--- init.lua
+++ init.lua
@@ -847,7 +847,8 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'base16-outrun-dark'
+      -- vim.cmd.colorscheme 'base16-outrun-dark'
+      vim.cmd.colorscheme 'delek'
     end,
   },
 
